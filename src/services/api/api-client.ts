67395798ec654855a76639ee56413643a3450c5f
mjekott <ekottmfon@yahoo.com>
2023-03-26 15:58:32 +0100

    build(API_KEY): moved api key to env

--- src/services/api/api-client.ts
+++ src/services/api/api-client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosClient = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "4acba2972c9a4f86940cd1d4d0106279",
+    key: import.meta.env.VITE_API_KEY,
   },
 });
 
